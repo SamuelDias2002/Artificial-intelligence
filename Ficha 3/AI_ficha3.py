1719e44073f3f8f11a0e64e03a8fc3de2c367a6c
Samuel Dias <95585194+samuel2002dias@users.noreply.github.com>
2023-10-09 19:30:58 +0100

    9/10/23

--- Ficha 3/AI_ficha3.py
+++ Ficha 3/AI_ficha3.py
@@ -2,78 +2,63 @@
 import copy
 import random
 
-# ------------------------------------------------------------------
 
-
+# Imprime tabuleiro no terminal
 def mostra_tabuleiro(T):
     for i in range(0, 3):
         for j in range(0, 3):
-            action = T[i+j]
+            action = T[i * 3 + j] # O cálculo i * 3 + j converte as coordenadas (linha, coluna) em um índice na lista T, 
+                                  # permitindo acessar o valor naquela posição do tabuleiro.
             if action == 0:
-                print("-", end="")
+                print("-", end=" ")
             elif action == 1:
-                print("X", end="")
+                print("X", end=" ")
             elif action == -1:
-                print("O", end="")
+                print("O", end=" ")
+        print()
 
-
-# ------------------------------------------------------------------
-# devolve a lista de ações que se podem executar partido de um estado
+# Devolve a lista de ações que se podem executar partido de um estado
 def acoes(T):
     states = []
-    jogadas = len(T) - T.count(0)
-    nMax = T.count(1)
-    nMin = T.count(-1)
-    # jogadas_maximas = 9
-    if jogadas == 0:
-        for i in range(0, 9):
-            if T[i] == 0:
-                states.append(i)
+    for i in range(0, 9):
+        if T[i] == 0:
+            states.append(i)
     return states
 
-# ------------------------------------------------------------------
-# devolve o estado que resulta de partir de um estado e executar uma ação
-
-
+# Devolve o estado que resulta de partir de um estado e executar uma ação
 def resultado(T, a, jog):
     aux = copy.copy(T)
-    # COMPLETAR
+    if aux[a] == 0:
+        if jog == 'MAX':
+            aux[a] = 1
+        elif jog == 'MIN':
+            aux[a] = -1
     return aux
-
-# ------------------------------------------------------------------
-# existem 8 possíveis alinhamentos vencedores, para cada jogador
 
 
 def utilidade(T):
-    # testa as linhas
+    # Testa as linhas
     for i in (0, 3, 6):
-        if T[i] != 0:
-            if T[i] == T[i+1] and T[i] == T[i+2]:
-                return T[i]
-    # testa as colunas
+        if T[i] != 0 and T[i] == T[i + 1] and T[i] == T[i + 2]:
+            return T[i]
+    # Testa as colunas
     for i in (0, 1, 2):
-        if T[i] != 0:
-            if T[i] == T[i+3] and T[i] == T[i+6]:
-                return T[i]
-    # testa as diagonais
-    if T[0] == T[4] == T[8] or T[2] == T[4] == T[6] and T[0] != 0:
-        return T[0]
-    # não é nodo folha ou dá empate
-    return 0
+        if T[i] != 0 and T[i] == T[i + 3] and T[i] == T[i + 6]:
+            return T[i]
+    # Testa as diagonais
+    if (T[0] == T[4] == T[8] or T[2] == T[4] == T[6]) and T[4] != 0:
+        return T[4]
+    # Não é nodo folha ou dá empate
+    if 0 not in T:
+        return 0
+    return None
 
-# ------------------------------------------------------------------
-# devolve True se T é terminal, senão devolve False
-
+# Devolve True se T é terminal, senão devolve False
 
 def estado_terminal(T):
-    if utilidade(T) != 0:
-        return True
-    else:
-        return False
+    return utilidade(T) is not None or all(pos != 0 for pos in T)
 
-
-# ------------------------------------------------------------------
-# algoritmo da wikipedia
+# Algoritmo Alpha-Beta
 # https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning
 # ignoramos a profundidade e devolvemos o valor, a ação e o estado resultante
 def alfabeta(T, alfa, beta, jog):
@@ -105,7 +90,6 @@ def alfabeta(T, alfa, beta, jog):
         return v, ba, resultado(T, ba, 'MIN')
 # ------------------------------------------------------------------
 
-
 def joga_max(T):
     v, a, e = alfabeta(T, -10, 10, True)
     print("MAX joga para", a)
@@ -122,10 +106,8 @@ def joga_min(T):
 
 
 def jogo(p1, p2):
-    # cria tabuleiro vazio
+# Cria tabuleiro vazio
     T = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    # podemos partir de um estado mais "avançado"
-    # T = [1,-1,0,0,-1,0,1,0,0]
     mostra_tabuleiro(T)
     while acoes(T) != [] and not estado_terminal(T):
         T = p1(T)
@@ -133,7 +115,7 @@ def jogo(p1, p2):
         if acoes(T) != [] and not estado_terminal(T):
             T = p2(T)
             mostra_tabuleiro(T)
-    # fim
+# End game
     if utilidade(T) == 1:
         print("Venceu o jog1")
     elif utilidade(T) == -1:
@@ -141,24 +123,37 @@ def jogo(p1, p2):
     else:
         print("Empate")
 
+
+
 # ------------------------------------------------------------------
 # jogador aleatório
 
 
 def joga_rand(T):
-    x = random.randint(0, 8)
-    if T[x] != 0:
-        while T[x] != 0:
-            x = random.randint(0, 8)
-            T[x] = -1
-    # COMPLETAR
+    acoes_disponiveis = acoes(T)
+    if acoes_disponiveis:
+        x = random.choice(acoes_disponiveis)
+        T[x] = 1
     return T
 
 # ------------------------------------------------------------------
+# Exercicio 2 - Jogador Humano
+def joga_humano(T):
+    actions_avaliable = acoes(T)
+    if actions_avaliable:
+        while True:
+            acao = int(input("Escolhe uma casa para jogar (0-8): "))
+            if acao in actions_avaliable:
+                T[acao] = -1
+                return T
+            else:
+                print("Ação inválida. Escolha uma casa disponível.")
+    return T
+
 # main
-
-
 # deve ganhar sempre o max:
-jogo(joga_max, joga_rand)
+#jogo(joga_max, joga_rand)
+jogo (joga_max, joga_humano)
+#jogo(joga_humano, joga_rand)
 # devem empatar sempre:
-# jogo(joga_max,joga_min)
+#jogo(joga_max,joga_min)
